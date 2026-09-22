eec9583326283dbe5bcc7bd571b76c06ba4e40c1
Mrerg7 <87950693+Mrerg7@users.noreply.github.com>
2026-09-22 08:21:59 -0700

    Collapse http/www redirects into a single hop

--- worker.js
+++ worker.js
@@ -1,14 +1,19 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
+    let redirect = false;
 
     if (url.protocol === "http:") {
       url.protocol = "https:";
-      return Response.redirect(url.toString(), 301);
+      redirect = true;
     }
 
     if (url.hostname === "www.computeraidedcomposition.com") {
       url.hostname = "computeraidedcomposition.com";
+      redirect = true;
+    }
+
+    if (redirect) {
       return Response.redirect(url.toString(), 301);
     }
 
